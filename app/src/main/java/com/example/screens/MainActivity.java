019502c9b87e59058a9cd9620f0de0b73aec9bc3
f22andbe <f22andbe@student.his.se>
2023-05-12 16:39:22 +0200

    Setup handler in main activity that switches to video activity

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tw;
+    EditText urlField;
+    Button switchActivity;
+    static final String defaultUrl="https://www.youtube.com/watch?v=LIUoFuSuvTM&ab_channel=NeringaRekasiute";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tw = findViewById(R.id.message);
+        urlField = findViewById(R.id.url_field);
+        switchActivity = findViewById(R.id.switch_activity);
+    }
+
+    public void onUrlFieldClick(View view) {
+
+    }
+
+    public void onButtonClick(View view) {
+        Intent switchIntent = new Intent(this, VideoActivity.class);
+        String url = urlField.getText().toString();
+        if(url.isEmpty()) {
+            switchIntent.putExtra("url", defaultUrl);
+        }else {
+            switchIntent.putExtra("url", url);
+        }
+
+        startActivity(switchIntent);
     }
 }
