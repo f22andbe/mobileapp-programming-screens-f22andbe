d46ff3ab73e2d60a472ddcf05758fe186c88c11c
f22andbe <f22andbe@student.his.se>
2023-05-13 00:55:44 +0200

    Setup webview and webviewclient, extract data from intent through which the activity was launched

--- app/src/main/java/com/example/screens/VideoActivity.java
+++ app/src/main/java/com/example/screens/VideoActivity.java
@@ -1,25 +1,57 @@
 package com.example.screens;
 
+import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import java.io.IOException;
 
 public class VideoActivity extends AppCompatActivity {
 
-    TextView tw;
+    TextView textview;
+    Intent intent;
+    private WebView webview;
+    private WebSettings webSettings;
+
 
     @Override
-    protected void onCreate(Bundle SavedInstanceState){
+    protected void onCreate(Bundle SavedInstanceState) {
         super.onCreate(SavedInstanceState);
         setContentView(R.layout.activity_video);
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
 
+        intent = getIntent();
+        webview = findViewById(R.id.youtube);
+        webview.setWebViewClient(new WebViewClient());
+        webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setDomStorageEnabled(true);
+
+        String url = intent.getStringExtra("url"); // your URL here
+        Log.i("VideoActivity", "url from intent is " + url);
+        webview.loadUrl(url);
     }
 
+
     @Override
-    public void onBackPressed(){
+    public void onBackPressed() {
+        if(webview.canGoBack())
+            webview.goBack();
+        else
             super.onBackPressed();
+
     }
 
 }
